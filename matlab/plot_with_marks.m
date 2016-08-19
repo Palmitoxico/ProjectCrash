28de291adc3796110097035ebd35f1bbf1be9f16
Tiago <tiagoft@gmail.com>
2016-08-19 15:58:09 -0300

    Function to plot beat marks
    Added a function to plot signals with corresponding beat marks.
    The function supports a beat mark tolerance, so we can check if our results are within an acceptable range.
    The default tolerance was configured to be +-0.01s.

---
+++ matlab/plot_with_marks.m
@@ -0,0 +1,31 @@
+function f = plot_with_marks(y, fs, marks, tolerance)
+% Plots signal y and adds marks.
+%
+% Inputs:
+% y: signal, in a one-dimensional array
+% fs: sampling frequency of y
+% marks: array of time (in seconds) marks
+% tolerance: allowed tolerance for time marks (default: 0.01)
+
+if nargin < 4
+  tolerance = 0.01
+end
+
+ymax = max(y);
+ymin = min(y);
+
+t = 1 : length(y);
+t = (t-1)/fs;
+
+figure;
+plot(t, y);
+
+for i = 1 : length(marks)
+  line( [marks(i)-tolerance, marks(i)+tolerance], [ymax, ymax]);
+  line( [marks(i)-tolerance, marks(i)+tolerance], [ymin, ymin]);
+  line( [marks(i)-tolerance, marks(i)-tolerance], [ymax, ymin]);
+  line( [marks(i)+tolerance, marks(i)+tolerance], [ymax, ymin]);
+end
+
+end
+
