8d29c3e7b489947ce5adb3b77ceba7307259b9c7
Gustavo Nishihara <gustavonishihara@hotmail.com>
2016-09-05 15:54:19 -0300

    Bands stored in matrix

--- matlab/operation.m
+++ matlab/operation.m
@@ -3,7 +3,6 @@ n = size(FILT, 2);
 for i = 1 : n/2 % creates filters and performs operation
     odd = 1:2:n;
     even = 2:2:n;
-    result = 0;
     [B, A] = butter(4,FILT([odd(i) even(i)]));
     band = filter(B,A,data); % butterworth bandpass filter
     band  = abs(band); % full wave rectifier
@@ -11,13 +10,12 @@ for i = 1 : n/2 % creates filters and performs operation
     band = filter(B,A,band); % butterworth lowpass filter
     band = diff(band); % first order difference
     band(band<0) = 0; % half wave rectifier
-    if size(result,2) < size(band,2)
+    band = transpose(band);
+    if i == 1
         result(size(band,2)) = 0;
-    elseif size(band,2) < size(result,2)
-        band(size(result,2)) = 0;
     end
-    result = result + band; % sum
+    result(i,:) =  band; % store bands in matrix
 end
-
 end
+%% 
 
