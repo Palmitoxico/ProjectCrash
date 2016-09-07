c0416e5856cb285c3e65ea7154303f915cadc614
anacso17 <anacso1777@gmail.com>
2016-09-07 15:21:56 -0300

    Update evaluate.m

--- matlab/evaluate.m
+++ matlab/evaluate.m
@@ -6,47 +6,44 @@
 %
 % y : array of found onsets 
 % d : ground truth array
-% R : number of correct results in d
-% r : R / lenght(d)
-% P : number of correct results in y
-% p : P / lenght(y)
+% h : number of correct results
+% r : h / lenght(d)
+% p : h / lenght(y)
 %
 
 function [r, p, F] = evaluate(y, d) 
 
-% tolerance
-e = 0.2;
+% tolerance in ms
+e = 20;
 
 % order the arrays
 y = sort(y);
 d = sort(d);
 
 % initialize the variables to the loop
-R = 0;
-P = 0;
+h = 0;
 j = 1;
 i = 1;
 ly = length(y);
 ld = length(d);
 
-while ( i <= ly ) && ( j <= ld )
-   if ( norm(d(j) - y(i)) <= e )
-       R = R + 1;
-       P = P + 1;
+while ( i <= ly ) && ( j <= ld ) 
+   if ( norm(d(j) - y(i)) <= e ) % if its a hit
+       h = h + 1;
        j = j + 1;
        i = i + 1;
 
-   elseif ( norm(d(j) - y(i)) > e ) && ( d(j) < y(i) )
+   elseif ( norm(d(j) - y(i)) > e ) && ( d(j) < y(i) ) % if its a mistake in d
             j = j + 1;
 
-   elseif ( norm(d(j) - y(i)) > e ) && ( d(j) > y(i) )  
+   elseif ( norm(d(j) - y(i)) > e ) && ( d(j) > y(i) ) % if its a mistake in y
             i = i + 1;
   
    end      
 end
 
-r = R/ld;
-p = P/ly;
+r = h/ld;
+p = h/ly;
 F = 2*p*r/(p+r);
 
 end
