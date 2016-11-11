89ff950202ba598ee465121911a4b386b7840633
Gustavo Nishihara <gustavonishihara@hotmail.com>
2016-11-11 11:10:45 -0200

    Added evaluator for competition

--- matlab/make_and_eval.m
+++ matlab/make_and_eval.m
@@ -1,4 +1,4 @@
-function f = make_and_eval(directory)
+function [f, mainscore] = make_and_eval(directory)
   d = dir([directory '/*.wav']);
   d2 = dir([directory '/*.txt']);
   f0 = [];
@@ -8,6 +8,10 @@ function f = make_and_eval(directory)
     bpm = f(1);
     fase = f(2);
     t = bpm_fase_beats(bpm, fase);
+    %FID = fopen('/Users/gustavonishihara/Downloads/train_teste/teste/teste.txt','w');
+    %fprintf(FID,'%f\n',t);
+    %fclose(FID);
+    [mainscore , backupscore] = beatEvaluator(t, ref);
     [r, p, f] = evaluate(t, ref);
     f0 = [f0 f];
   end;
