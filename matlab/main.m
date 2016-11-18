44d719df382ad1dd75d7eb7655eb4d961ced0f9e
Tiago Tavares <tiagoft@gmail.com>
2016-11-18 09:18:26 -0200

    Prepared main.m for refactoring

--- matlab/main.m
+++ matlab/main.m
@@ -1,2 +1,17 @@
 clear all
 close all
+
+% Calcular funcao de deteccao de onsets (ODF)
+
+% Dividir em quadros
+% Para cada quadro {
+
+%% Detectar BPM
+
+%% Detectar fase
+
+%% Extrapolar predicao
+
+%% }
+
+% Avaliar resultados
