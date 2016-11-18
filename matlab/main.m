49a9d066df27a23a15511f11552db67640c4a16d
Gustavo Nishihara <gustavonishihara@hotmail.com>
2016-11-18 10:06:12 -0200

    Added directory for dataset

--- matlab/main.m
+++ matlab/main.m
@@ -1,41 +1,42 @@
 clear all
 close all
+% Global variables
+file_length = 0; % number of samples within a file
+fs = 0; % sample rate of the file
+
 directory = '' % path to dataset
 wav_file = dir([directory '/*.wav']);
 truth_file = dir([directory '/*.txt']);
 for i = 1 : length(wav_file)
-    % Global variables
-    file_length = 0; % number of samples within a file
-    fs = 0; % sample rate of the file
     truth_table = load([directory  '/'  truth_file(i).name]);
     [audio fs bits] = wavread([directory  '/'  wav_file(i).name]);
+    [~ file_length] = size(audio);
+% Calcular funcao de deteccao de onsets (ODF)
+h = odf(audio,fs, n_filters, init_freq, fator);
 
-    % Calcular funcao de deteccao de onsets (ODF)
-    h = odf(audio,fs, n_filters, init_freq, fator);
+% Divide ODF in frames
+frame_length = 5; % Frame length, in seconds
+frame_step = 2.5; % Frame overlap, in seconds
 
-    % Divide ODF in frames
-    frame_length = 5; % Frame length, in seconds
-    frame_step = 2.5; % Frame overlap, in seconds
+frame_length_samples = frame_length * fs; % Samples per frame
+frame_step_samples = frame_step * fs; % Overlapping frames
 
-    frame_length_samples = frame_length * fs; % Samples per frame
-    frame_step_samples = frame_step * fs; % Overlapping frames
-
-    n = 1;
-    % For each frame
-    while ((n*frame_step_samples) + frame_length_samples) < file_length
-      frame_odf = h(n*frame_step_samples : n*frame_step_samples + frame_length_samples);
-
-
-
-    %% BPM estimation
-    bpm = frame_bpm(frame_odf, fs);
-
-    %% Detectar fase
-
-    %% Extrapolar predicao
+n = 0;
+% For each frame
+while ((n*frame_step_samples) + frame_length_samples) < file_length
+  frame_odf = h(n*frame_step_samples + 1: n*frame_step_samples + frame_length_samples);
 
 
-    end;
 
+%% BPM estimation
+bpm = frame_bpm(frame_odf, fs);
+
+%% Detectar fase
+
+%% Extrapolar predicao
+
+  n = n + 1; % Next frame;
 end;
+end;
+
 % Avaliar resultados
