2f840a76e88495e2ebbaefb039a0ac91dd9d5f20
Tiago Tavares <tiagoft@gmail.com>
2016-11-18 10:35:30 -0200

    Added help information to predict_beats.m

--- matlab/predict_beats.m
+++ matlab/predict_beats.m
@@ -1,4 +1,16 @@
 funtion p = predict_beats(bpm, phase, t0, tmin, tmax)
+% p = predict_beats(bpm, phase, t0, tmin, tmax)
+%
+% Predicts beats in future time based on current analysis
+%
+% Inputs:
+% bpm - beats per minute
+% t0 - start of current frame
+% phase - delay (seconds) between start of current frame and next beat
+% tmin, tmax - boundaries within we wish to yield predicted beats
+%
+% Outputs:
+% p - array of beat locations (in seconds)
 
 bps = bpm/60;
 spb = 1/bps;
