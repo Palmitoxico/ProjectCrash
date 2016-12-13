9628ba392f9760e86bd524851d7cfa3f98a4eec4
Tiago <tiagoft@gmail.com>
2016-12-13 16:18:39 -0200

    [works] Seems to work at this point with reasonable results

--- matlab/realtime/operation.m
+++ matlab/realtime/operation.m
@@ -1,4 +1,4 @@
-function result = operation(data,FILT)
+function result = operation(data,FILT, fs)
 
 n = size(FILT, 2);
 
@@ -6,16 +6,18 @@ for i = 1 : n/2                     % creates filters and performs operation
     odd = 1:2:n;
     even = 2:2:n;
     result = 0;
-    
-    [B, A] = butter(4,FILT([odd(i) even(i)]));
+
+    [B, A] = butter(2,FILT([odd(i) even(i)]));
+
     band = filter(B,A,data);        % butterworth bandpass filter
     band  = abs(band);              % full wave rectifier
-    
-    [B, A] = butter(4, 0.000907,'low');
+
+
+    [B, A] = butter(4, 40/fs,'low');
     band = filter(B,A,band);        % butterworth lowpass filter
     band = diff(band);              % first order difference
     band(band<0) = 0;               % half wave rectifier
-    
+
     if size(result,2) < size(band,2)
         result(size(band,2)) = 0;
     elseif size(band,2) < size(result,2)
