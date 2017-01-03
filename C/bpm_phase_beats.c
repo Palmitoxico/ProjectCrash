01a6aab3e3f2ecee71faf1d32cad714ad5cf3021
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-01-02 23:24:45 -0200

    Added MarkBeat()

--- C/bpm_phase_beats.c
+++ C/bpm_phase_beats.c
@@ -1,14 +1,10 @@
 void bpm_phase_beats(float32_t bpm, float32_t phase, uint32_t onsets_size, uint32_t fs, float32_t *beats)
 {
 	float32_t i = phase;
-	uint32_t j = 0;
 	float32_t beats_period = 1/(bpm/60);
 	while(i < onsets_size/fs)
 	{
-		{
-			beats[j] = i;
-			j = j + 1;
-			i = i + beats_period;
-		}
+		MarkBeat((uint32_t)i*1000);
+		i = i + beats_period;
 	}
 }
