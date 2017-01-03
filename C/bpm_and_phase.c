68ac08688ca5a15ce50ae267f36fbfbd45f3b867
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-01-02 23:31:35 -0200

    bpm_and_phase_test.m to C

--- C/bpm_and_phase.c
+++ C/bpm_and_phase.c
@@ -1,11 +1,11 @@
-float32_t bpm_and_phase(float32_t *onsets, uint32_t fs, uint32_t size, uint32_t flag_result)
+float32_t bpm_and_phase(float32_t *onsets, uint32_t onsets_size, uint32_t fs, uint32_t size, uint32_t flag_result)
 {
 	float32_t n;
 	float32_t phase;
 	float32_t bpmfs = bpm(onsets, fs, size);
 	if(bpmfs != 0)
 	{
-		n = phase(fs*(1/bpmf/60), onsets);
+		n = phase(fs*(1/bpmf/60), onsets, onsets_size);
 		phase = n/fs;
 	}
 	else
