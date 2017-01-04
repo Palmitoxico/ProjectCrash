2a0ba050876bc845f2b60d8393d0360560d13b19
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-01-03 22:19:15 -0200

    Added onsets detection

--- C/bpm_and_phase.c
+++ C/bpm_and_phase.c
@@ -1,12 +1,50 @@
-float32_t bpm_and_phase(float32_t *onsets, uint32_t onsets_size, uint32_t fs, uint32_t size, uint32_t flag_result) // flag to return phase or final bpm. Default to bpm
+float32_t bpm_and_phase(int8_t audio, int8_t flag_result) // flag to return phase or final bpm. Default to bpm
 {
 	float32_t n;
 	float32_t phase;
-	float32_t bpmfs = bpm(onsets, fs, size);
+	int8_t buffer[1000][32];
+	int8_t energy[1000];
+	int8_t onsets[1000];
+	energy[0] = 0;
+	int8_t i, j, k;
+	for(i = 0; i<1000; i++)
+	{
+		for (j = 0; j < 32; j++)
+		{
+			buffer[i][j] = audio[k];
+			k += 1;
+		}
+	}
+
+	for (i = 0; i < 1000; i++)
+	{
+		for (j = 0; j < 32; j++)
+		{
+			if (buffer[i][j] < 0)
+			{
+				buffer[i][j] = buffer[i][j]*(-1);
+			}
+			
+			energy[i] = energy[i] + buffer[i][j]**2;
+		}
+	}
+
+	for (i = 0; i < 999; i++)
+	{
+		onsets[i] = energy[i+1] - energy[i];
+		if (onsets[i] < 0)
+		{
+			onsets[i] = 0;
+		}
+	}
+
+	// apply a low pass filter in array onsets at 12,5Hz
+
+	float32_t bpmfs = bpm(onsets, 250, 999);
 	if(bpmfs != 0)
 	{
-		n = phase(fs*(1/bpmf/60), onsets, onsets_size);
-		phase = n/fs;
+		n = phase(250*(1/(bpmf/60)), onsets, onsets_size);
+		phase = n/250;
 	}
 	else
 	{
